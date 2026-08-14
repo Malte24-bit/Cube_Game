ce01977cc3b7c99ab4549e5e415b211be00d29d8
Malte <maosw24@outlook.de>
2026-08-14 21:15:22 +0200

    added movement

--- Cube.py
+++ Cube.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+
 verticies = (
     (-0.5,  0.5,  0.5),
     (-0.5,  0.5, -0.5),
@@ -14,6 +15,7 @@ verticies = (
     (-0.5, -0.5,  0.5),
     ( 0.5, -0.5,  0.5)
 )
+
 
 edges = (
     (0, 1),
@@ -33,33 +35,89 @@ edges = (
 
 def Cube():
     glBegin(GL_LINES)
+
     for edge in edges:
         for vertex in edge:
             glVertex3fv(verticies[vertex])
+
     glEnd()
 
 
 def main():
+
     pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(45, display[0] / display[1], 0.1, 50.0)
+    display = (800, 600)
 
-    glTranslatef(0,0,-5)
+    pygame.display.set_mode(
+        display,
+        DOUBLEBUF | OPENGL
+    )
 
-    glRotatef(20,0,0,0)
-    
+    # Perspektive
+    gluPerspective(
+        45,
+        display[0] / display[1],
+        0.1,
+        50.0
+    )
+
+    glTranslatef(0, 0, -5)
+
+    # Spielerposition
+    cube_x = 0
+    cube_y = 0
+
+    # Spielgeschwindigkeit
+    speed = 0.05
+
+
     while True:
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            cube_y += speed
+
+        if keys[pygame.K_DOWN]:
+            cube_y -= speed
+
+        if keys[pygame.K_LEFT]:
+            cube_x -= speed
+
+        if keys[pygame.K_RIGHT]:
+            cube_x += speed
+
+
+        glClear(
+            GL_COLOR_BUFFER_BIT |
+            GL_DEPTH_BUFFER_BIT
+        )
+
+
+        glPushMatrix()
+
+        # Cube an Spielerposition bewegen
+        glTranslatef(
+            cube_x,
+            cube_y,
+            0
+        )
+
         Cube()
+
+        glPopMatrix()
+
         pygame.display.flip()
+
         pygame.time.wait(10)
+
 
 main()
 quit()
